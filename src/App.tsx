b9d3bd9577569d22a9dffb41089d81bec41e2e62
Alexis Salgado <chimalexoficial@gmail.com>
2024-10-22 16:28:59 -0600

    showing items, to add tip

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import MenuItem from "./components/MenuItem"
+import OrderContents from "./components/OrderContents";
+import OrderTotals from "./components/OrderTotals";
 import { menuItems } from "./data/db"
+import useOrder from "./hooks/useOrder"
 
 function App() {
+
+  const { order, addItem, removeItem } = useOrder();
 
   return (
     <>
@@ -9,12 +15,29 @@ function App() {
       </header>
 
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div>
-          <h2>Menu</h2>
+        <div className="p-5">
+          <h2 className="text-4xl font-bold">Menu</h2>
+
+          <div className="space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItem
+                key={item.id}
+                item={item}
+                addItem={addItem} />
+            ))}
+          </div>
+
         </div>
 
-        <div>
-          <h2>Your order</h2>
+        <div className="border border-dashed border-slate-300 rounded-lg space-y-10">
+
+          <OrderContents
+            order={order}
+            removeItem={removeItem} />
+
+          <OrderTotals
+            order={order} />
+
         </div>
       </main>
     </>
---
+++ src/components/OrderTotals.tsx
@@ -0,0 +1,36 @@
+import { useMemo } from "react"
+import { OrderItem } from "../types"
+import { formatCurrency } from "../helpers";
+
+
+type OrderTotalsProps = {
+    order: OrderItem[]
+}
+
+export default function OrderTotals({ order }: OrderTotalsProps) {
+
+    const subtotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order]);
+
+    return (
+        <>
+            <div className="space-y-3">
+                <h2 className="font-black text-2xl"></h2>
+                <p>Subtotal: {' '}
+                    <span className="font-bold">{formatCurrency(subtotalAmount)}</span>
+                </p>
+
+                <p>Tip: {' '}
+                    <span className="font-bold">$0</span>
+                </p>
+
+                <p>Total to pay: {' '}
+                    <span className="font-bold">$0</span>
+                </p>
+            </div>
+
+            <button>
+
+            </button>
+        </>
+    )
+}
